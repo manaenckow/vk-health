54a2cdccf547d8e327aa1efbce5d2e7613fdda09
Maxim <48844646+manaenckow@users.noreply.github.com>
2020-01-10 03:59:38 +0300

    Updates

---
+++ precache-manifest.4763c847491d263005b9e3ba514cb652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf04e11eef8a9d6594f",
+    "url": "/vk-health/static/css/main.f41b8c42.chunk.css"
+  },
+  {
+    "revision": "1bf04e11eef8a9d6594f",
+    "url": "/vk-health/static/js/main.97127f19.chunk.js"
+  },
+  {
+    "revision": "9d5325d23fad0733ee41",
+    "url": "/vk-health/static/js/runtime~main.2561c943.js"
+  },
+  {
+    "revision": "83c3eb2c8c427f12454b",
+    "url": "/vk-health/static/css/2.d6e1e4a5.chunk.css"
+  },
+  {
+    "revision": "83c3eb2c8c427f12454b",
+    "url": "/vk-health/static/js/2.ec4201c0.chunk.js"
+  },
+  {
+    "revision": "2b9a2f4a80aea8189493e6522e4f93ff",
+    "url": "/vk-health/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/vk-health/precache-manifest.4763c847491d263005b9e3ba514cb652.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/vk-health/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
